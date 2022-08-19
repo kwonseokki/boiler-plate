239101907c60912d9d39fd6917fbb71c759b68fb
seokki kwon <kwons4163@naver.com>
2022-08-20 07:45:16 +0900

    setting key

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express"); // express 모듈 가져오기
 const app = express(); // 새로운 express 앱생성
 const port = 5000; // 포트번호지정
-
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+//application/x-www.form.urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
+const config = require("./config/key");
 mongoose
-  .connect(
-    "mongodb+srv://admin:1234@react-node.bgg018w.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => {
     console.log("mongoDB connected!");
   })
@@ -15,7 +19,20 @@ mongoose
   });
 
 app.get("/", (req, res) => {
-  res.send("Hello World! 노드 실행");
+  res.send("Hello World! 노드 실행d");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어줌
+
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
